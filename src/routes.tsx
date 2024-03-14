60b83c224293fc4342ad2e9dffc408fc1aea620d
peterjuma <30427827+peterjuma@users.noreply.github.com>
2024-03-14 15:47:12 +0300

    Add vulnerability

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,14 +4,21 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import landing from './pages/landing';
 import Resume from './pages/Resume';
 
- function Routes(){
-   return(
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Route path="/" exact component={landing} />
-        <Route path="/app" component={Resume} />
-      </BrowserRouter>
+// Added component that introduces an XSS vulnerability
+const UnsafeComponent = () => {
+  const badInnerHTML = "<script>alert('XSS!');</script>";
+  return <div dangerouslySetInnerHTML={{__html: badInnerHTML}} />;
+};
 
-   );
- } 
+function Routes(){
+  return(
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Route path="/" exact component={landing} />
+      <Route path="/app" component={Resume} />
+      {/* Route using the unsafe component */}
+      <Route path="/unsafe" component={UnsafeComponent} />
+    </BrowserRouter>
+  );
+}
 
- export default Routes;
+export default Routes;
